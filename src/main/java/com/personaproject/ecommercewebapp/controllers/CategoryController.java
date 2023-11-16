6da04b037fbd2ac76b1b1069ce6c0699faceddb9
davidajimati <ajimatidavid@gmail.com>
2023-11-16 16:45:40 +0100

    created methods to create, update and findAll categories in the DB

--- src/main/java/com/personaproject/ecommercewebapp/controllers/CategoryController.java
+++ src/main/java/com/personaproject/ecommercewebapp/controllers/CategoryController.java
@@ -1,44 +1,46 @@
 package com.personaproject.ecommercewebapp.controllers;
 
 import com.personaproject.ecommercewebapp.dtos.CategoryDTO;
-import com.personaproject.ecommercewebapp.enums.ResponseMessages;
+import com.personaproject.ecommercewebapp.entity.Category;
 import com.personaproject.ecommercewebapp.enums.StandardServiceTokens;
 import com.personaproject.ecommercewebapp.services.CategoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-
+import java.util.List;
 import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/category")
 public class CategoryController {
 
     private final StandardServiceTokens standardServiceTokens;
     private final CategoryService categoryService;
 //    private ResponseMessages responseMessages;
 
-    @PostMapping("/store/create_category")
+    @PostMapping("/create")
     public String createNewCategory(@RequestBody CategoryDTO categoryDTO,
                                     @RequestHeader String serviceToken,
                                     @RequestHeader String authToken) throws IllegalAccessException {
-        boolean isServiceTokenSame = Objects.equals(serviceToken, standardServiceTokens.categoryToken);
-        boolean isAuthTokenSame = Objects.equals(authToken, standardServiceTokens.authToken);
-
-        System.out.println("\t\t |");
-        System.out.println("\t\t |");
-        System.out.println("check if service token is the same -> " + isServiceTokenSame);
-        System.out.println("check if service auth is the same -> " + isAuthTokenSame);
         System.out.println("\t\t |");
         System.out.println("\t\t |");
 
-        if (!isServiceTokenSame || !isAuthTokenSame) {
+        if (!Objects.equals(serviceToken, standardServiceTokens.categoryToken)
+                || !Objects.equals(authToken, standardServiceTokens.authToken)) {
             throw new IllegalAccessException("Wrong Service and/or authentication Token");
         }
         return (categoryService.createCategory(categoryDTO));
+    }
+
+    @GetMapping("/get_all")
+    public List<Category> categoryList() {
+        return categoryService.listAllCategories();
+    }
+
+    @PostMapping("/update/{categoryId}")
+    public String updateCategory(@PathVariable Long categoryId,
+                                 @RequestBody CategoryDTO categoryDTO) {
+        return categoryService.updateCategory(categoryId, categoryDTO);
     }
 }
--- src/main/java/com/personaproject/ecommercewebapp/services/CategoryService.java
+++ src/main/java/com/personaproject/ecommercewebapp/services/CategoryService.java
@@ -3,9 +3,12 @@ package com.personaproject.ecommercewebapp.services;
 import com.personaproject.ecommercewebapp.dtos.CategoryDTO;
 import com.personaproject.ecommercewebapp.entity.Category;
 import com.personaproject.ecommercewebapp.repository.CategoryRepo;
+import jakarta.persistence.Id;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +25,18 @@ public class CategoryService {
 
         categoryRepo.save(category);
         return (category.getCategoryName() + " Category created successfully!");
+    }
+
+    public List<Category> listAllCategories() {
+        return categoryRepo.findAll();
+    }
+
+    public String updateCategory(Long categoryId, CategoryDTO categoryDTO) {
+        Category categoryItem = categoryRepo.getReferenceById(categoryId);
+        categoryItem.setCategoryDescription(categoryDTO.getDescription());
+        categoryItem.setCategoryName(categoryDTO.getCategoryName());
+        categoryItem.setCategoryImageUrl(categoryDTO.getImageUrl());
+        categoryRepo.save(categoryItem);
+        return ("Category with ID " + categoryId + " updated!");
     }
 }
